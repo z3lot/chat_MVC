001e92ae123f522e50bbd76360493c5d9eeb300d
zelot <zelot@RGB>
2022-05-20 05:36:01 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Dependencies:
+
+> pip install playsound
+
+---
+
+**None**:*Project to learn python and git*
---
+++ main.py
@@ -0,0 +1,14 @@
+import socket
+import tkinter
+import threading
+from tkinter import ttk
+from playsound import playsound
+from view import View
+from model import SocketServer,SocketClient
+from controler import Controller
+
+def main():
+    app = Controller()
+
+if __name__ == "__main__":
+    main()
